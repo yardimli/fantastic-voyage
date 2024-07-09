e77befe6154fae26553cd1b324c3ce48f54b9dda
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2024-07-09 14:13:39 +0800

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -18,6 +18,18 @@
 			$voices = MyHelper::fetchVoices($request);
 			$voices = json_decode($voices, true);
 
+			$good_voices = ['Rachel','Drew','Clyde','Domi','Fin','Sarah','Antoni','Elli','Liam','Arnold','Matilda','Jeremy','Freya','Grace','Adam','Nicole'];
+			//filter out the good voices from $voice['name']
+			$voices = array_filter($voices['voices'], function($voice) use ($good_voices) {
+				//check if the voice name is in the good voices array, case insensitive match beginning
+				for ($i = 0; $i < count($good_voices); $i++) {
+					if (stripos($voice['name']??'', $good_voices[$i]) === 0) {
+						return true;
+					}
+				}
+			});
+			$voices = array('voices' => array_values($voices));
+
 			return view('quiz.index-new', compact('voices'));
 		}
 
